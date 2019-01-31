df3270cd765b77f13c448914da76abef98757f2c
visualstudio <visualstudio@230-PC15>
2019-01-31 15:50:57 -0500

    jott

--- manjot4/Program.cs
+++ manjot4/Program.cs
@@ -4,12 +4,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace manjot4
+namespace _31jan2019
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Countryside Ontario = new Countryside();
+            Ontario.run();
+        }
+        class Village
+        {
+            public static int numberofvillages = 0;
+            public Village nextvillage;
+            public Village previousvillage;
+            public string Villagename;
+            public bool isAstrildeHere = false;
+            public Village()
+            { Village.numberofvillages++; }
+        }
+        class Countryside
+        {
+            public Village Maple;
+
+            public Village Toronto;
+            public Village Ajax;
+            public Village Head;
+            public Village Tail;
+            public Village Temp;
+            public void run()
+            {
+                this.MapInitializer();
+                this.LookForAstrilde();
+            }
+            public void MapInitializer()
+            {
+                Maple = new Village();
+                Maple.Villagename = "Maple";
+                Maple.previousvillage = null;
+                Maple.isAstrildeHere = true;
+                Toronto = new Village();
+                Toronto.Villagename = "Toronto";
+                Toronto.previousvillage = null;
+                Ajax = new Village();
+                Ajax.Villagename = "Ajax";
+                Ajax.nextvillage = null;
+                Ajax.previousvillage = Toronto;
+                /*Ajax.isAstrildeHere = true*/
+
+
+
+
+
+            }
+            public void LookForAstrilde()
+            {
+                Head = Maple;
+                if (Head.isAstrildeHere)
+                {
+                    Console.WriteLine("Yeah! Astrilde is in " + Head.Villagename);
+                }
+                //while(true)
+                //{
+
+                //}
+            }
         }
     }
 }
